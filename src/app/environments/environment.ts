faa3da8985afcb29e40d965ccff376544ddd8449
semajame <jamesanquillano@gmail.com>
2024-10-14 16:31:46 +0800

    update

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   apiUrl: 'http://localhost:4000',
+//   // apiUrl: 'https://misty-hosiery-elk.cyclic.app/aabbq-api'
+// };
 export const environment = {
-  apiUrl: 'https://benedicto-scheduling-backend.onrender.com/',
+  apiUrl: 'https://benedicto-scheduling-backend.onrender.com',
   // apiUrl: 'https://misty-hosiery-elk.cyclic.app/aabbq-api'
 };
