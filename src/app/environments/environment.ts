ffd994481335def10aa766037083cc81a6638317
James Sevilla Anquillano <142220813+semajame@users.noreply.github.com>
2024-10-29 23:43:39 +0800

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // apiUrl: 'https://benedicto-scheduling-backend.onrender.com',
-  apiUrl: 'http://localhost:4000',
+  apiUrl: 'https://benedicto-scheduling-backend.onrender.com',
+ // apiUrl: 'http://localhost:4000',
 };
 
 // UPDATE ONLINE
