eea7dcf28676a0960a7c68eda31c98950ce85d66
semajame <jamesanquillano@gmail.com>
2024-11-26 21:15:47 +0800

    update

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -40,9 +40,7 @@ export class AccountService {
 
   logout() {
     // remove user from local storage and set current user to null
-    localStorage.removeItem('jwt'); // Optional: Clear additional tokens if needed
     localStorage.removeItem('user');
-    sessionStorage.clear(); // Clear session data if using sessionStorage
     this.userSubject.next(null);
     this.router.navigate(['/account/login']);
   }
